f03a6c1a48fd0dc1cd58b049e00e37a77e49d8ec
Shadrack <ltdpas4@gmail.com>
2024-10-16 17:11:39 +0100

    created new projects

---
+++ divid.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+                float a=10,b=15,c;
+                printf("\n Hello World");
+                c=a/b;
+                printf("\n a/b=%.2f",c);
+        return 0;
+}
+
